e84cde8289bd24f82992df314ad03ddc071d84ad
broocksd <59925526+broocksd@users.noreply.github.com>
2020-01-15 10:04:48 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***********************************************
+* Hello.java
+************************************************
+* Destinee Broocks
+* January 15th 2020
+* 225 003
+************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Destinee Broocks>");
+		System.out.println("<Hello.java>");
+		System.out.println("<First Project>");
+		System.out.println("<2019 12 05>");
+		System.out.println("<225 003>");
+		System.out.println();
+	}
+}
